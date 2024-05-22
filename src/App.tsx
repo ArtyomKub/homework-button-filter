beb97b846177f9fd8b22f2614b688a4d66d43b4f
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-22 22:55:12 +0200

    Migrate code for filter in new components FilterFotMoney.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -31,22 +31,7 @@ function App() {
     }
 
     return (
-        <>
-            <ul>
-                {currentMoney.map((objectFromMoneyArray, index) => {
-                    return (
-                        <li key={index}>
-                            <span>{objectFromMoneyArray.banknots} - </span>
-                            <span>{objectFromMoneyArray.number} - </span>
-                            <span>{objectFromMoneyArray.value}</span>
-                        </li>
-                    )
-                })}
-            </ul>
-            <button onClick={() => onClickFilteredHandler('All')}>ALL MONEY</button>
-            <button onClick={() => onClickFilteredHandler('Dollars')}>DOLLARS</button>
-            <button onClick={() => onClickFilteredHandler('Rubles')}>RUBLES</button>
-        </>
+
     );
 }
 
--- src/components/FilterFotMoney.tsx
+++ src/components/FilterFotMoney.tsx
@@ -2,8 +2,21 @@ import React from 'react';
 
 export const FilterForMoney = () => {
     return (
-        <div>
-
-        </div>
+        <>
+            <ul>
+                {currentMoney.map((objectFromMoneyArray, index) => {
+                    return (
+                        <li key={index}>
+                            <span>{objectFromMoneyArray.banknots} - </span>
+                            <span>{objectFromMoneyArray.number} - </span>
+                            <span>{objectFromMoneyArray.value}</span>
+                        </li>
+                    )
+                })}
+            </ul>
+            <button onClick={() => onClickFilteredHandler('All')}>ALL MONEY</button>
+            <button onClick={() => onClickFilteredHandler('Dollars')}>DOLLARS</button>
+            <button onClick={() => onClickFilteredHandler('Rubles')}>RUBLES</button>
+        </>
     );
 };
