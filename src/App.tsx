c7761cfe345bc5b32329a321fb19f8a2571913bf
Artyom Kubyshkin <coder.kub.lover@gmail.com>
2024-05-22 22:22:03 +0200

    Create a list. Add money array in list (map)

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
 import React, {useState} from 'react';
 import './App.css';
 
-const [money, setMoney] = useState([
-    {banknots: 'Dollars', value: 100, number: ' a1234567890'},
-    {banknots: 'Dollars', value: 50, number: ' z1234567890'},
-    {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
-    {banknots: 'Dollars', value: 100, number: ' e1234567890'},
-    {banknots: 'Dollars', value: 50, number: ' c1234567890'},
-    {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
-    {banknots: 'Dollars', value: 50, number: ' x1234567890'},
-    {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
-])
-
 
 function App() {
+
+    const [money, setMoney] = useState([
+        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknots: 'Rubles', value: 100, number: ' w1234567890'},
+        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknots: 'Rubles', value: 100, number: ' r1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknots: 'Rubles', value: 50, number: ' v1234567890'},
+    ])
+
     return (
         <>
-
+            <ul>
+                {money.map((objectFromMoneyArray, index) => {
+                    return (
+                        <li key={index}>
+                            <span>{objectFromMoneyArray.banknots} - </span>
+                            <span>{objectFromMoneyArray.number} - </span>
+                            <span>{objectFromMoneyArray.value}</span>
+                        </li>
+                    )
+                })}
+            </ul>
         </>
     );
 }
